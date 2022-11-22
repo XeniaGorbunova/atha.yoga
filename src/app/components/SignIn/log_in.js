9166aa2b2fb3d8f4ad6c7eb779760a1cea650ee7
Xenia <GorbunovaKsenia@gmail.com>
2022-11-22 16:43:57 +0300

    LogIn form formated

--- src/app/components/SignIn/log_in.js
+++ src/app/components/SignIn/log_in.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
@@ -8,27 +7,13 @@ import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
 const theme = createTheme();
 
-export default function SignIn() {
+export default function LogIn() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -50,60 +35,63 @@ export default function SignIn() {
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
+          <Typography component="h1" variant="h2" class="header">
+            Войти в аккаунт
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
             <TextField
               margin="normal"
-              required
               fullWidth
               id="email"
-              label="Email Address"
+              placeholder="E-mail"
               name="email"
               autoComplete="email"
               autoFocus
             />
             <TextField
               margin="normal"
-              required
               fullWidth
               name="password"
-              label="Password"
+              placeholder="Пароль"
               type="password"
               id="password"
               autoComplete="current-password"
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
+            <Grid container alignItems="center">
+              <Grid item xs>
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Запомнить меня"
+                />
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Забыли пароль?
+                </Link>
+              </Grid>
+            </Grid>
             <Button
               type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign In
+              Войти
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
+            <Grid container spacing={1} alignItems="center" justifyContent="center">
+              <Grid item>
+                <Typography variant="body2">
+                  Ещё нет аккаунта?
+                </Typography>
               </Grid>
               <Grid item>
                 <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
+                  Зарегистрироваться
                 </Link>
               </Grid>
             </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </ThemeProvider>
   );
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import './style.scss';
+import './styles.scoped.scss';
 import CssBaseline from '@mui/material/CssBaseline';
-import SignIn from './components/SignIn/sign_in_default.js';
+import LogIn from './components/SignIn/log_in.js';
 
 export default function MyApp() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <SignIn />
+      <LogIn />
     </React.Fragment>
   );
 }
