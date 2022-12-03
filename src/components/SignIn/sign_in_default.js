68b99ed3fc02c1f2772de4e72fa3e5f121df1a1e
Xenia <GorbunovaKsenia@gmail.com>
2022-12-03 18:11:24 +0300

    debugging

--- src/components/SignIn/sign_in_default.js
+++ src/components/SignIn/sign_in_default.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
@@ -13,12 +13,12 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import Container from '@mui/material/Container';
 import { FormControl } from '@mui/material';
 import InputLabel from '@mui/material/InputLabel';
-import { useDispatch, useSelector } from "react-redux";
-import { register } from "../../slices/auth";
-import { clearMessage } from "../../slices/message";
+import { useDispatch, useSelector } from 'react-redux';
+import { register } from '../../slices/auth';
+import { clearMessage } from '../../slices/message';
 
 export default function SignInDefault() {
-    const Register = () => {
+  const Register = () => {
     const [successful, setSuccessful] = useState(false);
 
     const { message } = useSelector((state) => state.message);
@@ -28,143 +28,170 @@ export default function SignInDefault() {
       dispatch(clearMessage());
     }, [dispatch]);
 
-  const [values, setValues] = React.useState({
-    email: '',
-    amount: '',
-    password: '',
-    weight: '',
-    weightRange: '',
-    showPassword: false,
-  });
-
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
-
-  const handleClickShowPassword = () => {
-    setValues({
-      ...values,
-      showPassword: !values.showPassword,
+    const [values, setValues] = React.useState({
+      email: '',
+      amount: '',
+      password: '',
+      weight: '',
+      weightRange: '',
+      showPassword: false,
     });
-  };
 
-  const handleMouseDownPassword = (event) => {
-    event.preventDefault();
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-    const { email, password } = {
-      email: data.get('email'),
-      password: data.get('password'),
+    const handleChange = (prop) => (event) => {
+      setValues({
+        ...values,
+        [prop]: event.target.value,
+      });
     };
 
-    setSuccessful(false);
-
-    dispatch(register({ email, password }))
-      .unwrap()
-      .then(() => {
-        setSuccessful(true);
-      })
-      .catch(() => {
-        setSuccessful(false);
+    const handleClickShowPassword = () => {
+      setValues({
+        ...values,
+        showPassword: !values.showPassword,
       });
-  };
+    };
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Typography component="h1" variant="h4" sx={{ mb: 3 }}>
-          Регистрация
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }} className="form__container">
-          {!successful && (
-            <TextField
-            sx={{ mb: 2 }}
-            margin="normal"
-            fullWidth
-            id="email"
-            label="Электронная почта"
-            placeholder="E-mail"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            />
-            <FormControl variant="outlined" fullWidth>
-              <InputLabel>Пароль</InputLabel>
-              <OutlinedInput
-                sx={{ mb: 2 }}
-                fullWidth
-                label="Пароль"
-                name="password"
-                placeholder="Пароль"
-                id="password"
-                autoComplete="current-password"
-                type={values.showPassword ? 'text' : 'password'}
-                value={values.password}
-                onChange={handleChange('password')}
-                endAdornment={(
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                    >
-                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                )}
-              />
-            </FormControl>
-            <Button
-              type="submit"
-              size="large"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Зарегистрироваться
-            </Button>
-            <Grid container spacing={1} alignItems="center" justifyContent="center">
-              <Grid item>
-                <Typography variant="body2">
-                  Уже есть аккаунт?
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2" underline="none">
-                  Войти
-                </Link>
-              </Grid>
-            </Grid>
-          )}
-        </Box>
-        <div style={{ position: 'absolute', bottom: 32, textAlign: 'center', maxWidth: 380 }}>
-          <Typography variant="caption">
-            Нажимая на кнопку «Зарегистрироваться», я подтверждаю, что ознакомлен(а) с
-            <Link variant="caption" underline="none" marginLeft={1}>
-              пользовательским соглашением
-            </Link>
+    const handleMouseDownPassword = (event) => {
+      event.preventDefault();
+    };
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      const data = new FormData(event.currentTarget);
+      console.log({
+        email: data.get('email'),
+        password: data.get('password'),
+      });
+      const {
+        email,
+        password,
+      } = {
+        email: data.get('email'),
+        password: data.get('password'),
+      };
+
+      setSuccessful(false);
+
+      dispatch(register({
+        email,
+        password,
+      }))
+        .unwrap()
+        .then(() => {
+          setSuccessful(true);
+        })
+        .catch(() => {
+          setSuccessful(false);
+        });
+    };
+
+    return (
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h4" sx={{ mb: 3 }}>
+            Регистрация
           </Typography>
-        </div>
-      </Box>
-      {message && (
-        <div>
-          {message}
-        </div>
-      )}
-    </Container>
-  );
-};
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+            className="form__container"
+          >
+            {!successful && (
+            <>
+              <TextField
+                sx={{ mb: 2 }}
+                margin="normal"
+                fullWidth
+                id="email"
+                label="Электронная почта"
+                placeholder="E-mail"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <FormControl variant="outlined" fullWidth>
+                <InputLabel>Пароль</InputLabel>
+                <OutlinedInput
+                  sx={{ mb: 2 }}
+                  fullWidth
+                  label="Пароль"
+                  name="password"
+                  placeholder="Пароль"
+                  id="password"
+                  autoComplete="current-password"
+                  type={values.showPassword ? 'text' : 'password'}
+                  value={values.password}
+                  onChange={handleChange('password')}
+                  endAdornment={(
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  )}
+                />
+              </FormControl>
+              <Button
+                type="submit"
+                size="large"
+                fullWidth
+                variant="contained"
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                }}
+              >
+                Зарегистрироваться
+              </Button>
+              <Grid container spacing={1} alignItems="center" justifyContent="center">
+                <Grid item>
+                  <Typography variant="body2">
+                    Уже есть аккаунт?
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2" underline="none">
+                    Войти
+                  </Link>
+                </Grid>
+              </Grid>
+            </>
+            )}
+          </Box>
+          <div style={{
+            position: 'absolute',
+            bottom: 32,
+            textAlign: 'center',
+            maxWidth: 380,
+          }}
+          >
+            <Typography variant="caption">
+              Нажимая на кнопку «Зарегистрироваться», я подтверждаю, что ознакомлен(а) с
+              <Link variant="caption" underline="none" marginLeft={1}>
+                пользовательским соглашением
+              </Link>
+            </Typography>
+          </div>
+        </Box>
+        {message && (
+          <div>
+            {message}
+          </div>
+        )}
+      </Container>
+    );
+  };
+}
